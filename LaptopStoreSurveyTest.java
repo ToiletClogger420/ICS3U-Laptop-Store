bb508b25f180dbb3ed2245c3a807e32ee3a3ae51
Steven Chen <steven2381824724@gmail.com>
2024-11-27 13:53:45 -0500

    add print method into survey test

--- LaptopStoreSurveyTest.java
+++ LaptopStoreSurveyTest.java
@@ -2,12 +2,28 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
 import javax.swing.border.*;
+import java.util.Map;
 
 public class LaptopStoreSurveyTest {
     public static void main(String[] args) {
         javax.swing.SwingUtilities.invokeLater(new Runnable() {
             public void run() {
                 LaptopStoreSurveyFrame frame = new LaptopStoreSurveyFrame();
+                
+                // 添加窗口关闭监听器
+                frame.addWindowListener(new WindowAdapter() {
+                    @Override
+                    public void windowClosing(WindowEvent e) {
+                        // 获取并打印过滤结果
+                        Map<String, Object> filterResults = frame.getFilterValues();
+                        System.out.println("\n=== Filter Results ===");
+                        filterResults.forEach((key, value) -> {
+                            System.out.println(key + ": " + value);
+                        });
+                        System.out.println("===================\n");
+                    }
+                });
+                
                 frame.setVisible(true);
             }
         });
